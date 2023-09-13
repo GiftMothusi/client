6cfb509d51b34a604b521531d4d4bf59b0e1ddea
ThatoGift <giftgabanakgosi@gmail.com>
2023-09-13 10:44:02 +0200

    Project Structure and Components +Navbar finished

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SearchIcon from "@mui/icons-material/Search";
+import PersonOutlineOutlinedIcon from "@mui/icons-material/PersonOutlineOutlined";
+import FavoriteBorderOutlineIcon from "@mui/icons-material/FavoriteBorderOutlined";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+import { Link } from "react-router-dom";
+
+import "./Navbar.scss";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
+  return (
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left">
+          <div className="item">
+            <img src="/images/zar.png" alt="country_img" />
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <span>ZAR</span>
+            <KeyboardArrowDownIcon />
+          </div>
+          <div className="item">
+            <Link className="link" t="/products/1">
+              Women
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/2">
+              Men
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/products/3">
+              Children
+            </Link>
+          </div>
+        </div>
+
+        <div className="center">
+          <Link className="link" to="/">
+            The Plug
+          </Link>
+        </div>
+
+        <div className="right">
+          <div className="item">
+            <Link className="link" to="/">
+              Home
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              About
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              Contact
+            </Link>
+          </div>
+          <div className="item">
+            <Link className="link" to="/">
+              Stores
+            </Link>
+          </div>
+          <div className="icons">
+            <SearchIcon />
+            <PersonOutlineOutlinedIcon />
+            <FavoriteBorderOutlineIcon />
+            <div className="cartIcon" onClick={() => setOpen(!open)}>
+              <ShoppingCartOutlinedIcon />
+              <span></span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
